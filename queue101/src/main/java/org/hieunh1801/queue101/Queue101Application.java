d8dcb13a0881eb9c22ba743969668832e057d1d4
bob <bob@milvus.co.kr>
2025-04-17 20:48:05 +0900

    init

---
+++ queue101/src/main/java/org/hieunh1801/queue101/Queue101Application.java
@@ -0,0 +1,13 @@
+package org.hieunh1801.queue101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Queue101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Queue101Application.class, args);
+    }
+
+}
---
+++ queue101/src/test/java/org/hieunh1801/queue101/Queue101ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hieunh1801.queue101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Queue101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
