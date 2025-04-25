2a8e0ed41621692438f122f26ea8ca5b091e96da
bob <bob@milvus.co.kr>
2025-04-25 19:13:11 +0900

    TASK QUEUE

---
+++ springboot101/src/main/java/org/hieunh1801/springboot101/CAMEL_0002_TASK_QUEUE/processors/Cafe24Loader.java
@@ -0,0 +1,23 @@
+package org.hieunh1801.springboot101.CAMEL_0002_TASK_QUEUE.processors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@Component
+public class Cafe24Loader implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Map<String, Object> headers = exchange.getIn().getHeaders();
+        int taskId = (int) headers.get("taskId");
+        long runTime = ThreadLocalRandom.current().nextInt(1, 10);
+        log.info("taskId=[{}] Cafe24Loader started wait {}s", taskId, runTime);
+        Thread.sleep(runTime*1000);
+        log.info("taskId=[{}] Cafe24Loader ended", taskId);
+    }
+}
